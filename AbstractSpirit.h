bb76202e1186e01bbdc5c284fbb2511790797ec1
unknown <Administrator@Joy-Server.(none)>
2012-04-24 12:32:20 +0800

    try

---
+++ AbstractSpirit.h
@@ -0,0 +1,17 @@
+#ifndef ABSTRACT_SPIRIT_H
+#define ABSTRACT_SPIRIT_H
+namespace QtGameEngine
+{
+namespace RenderingEngine
+{
+class AbstractSpirit
+{
+public:
+
+
+};
+
+}
+
+}
+#endif // ABSTRACTSPIRIT_H
